1048d956c5d2a3664efe230ebdb06cfa6b0c0778
angiokop@gmail.com <angelomio1>
2020-12-03 23:35:32 +0100

    Splash aggiunto
    Signed-off-by: angiokop@gmail.com <angelomio1>

--- app/src/main/java/com/example/navigationbar/views/Splash.java
+++ app/src/main/java/com/example/navigationbar/views/Splash.java
@@ -2,15 +2,33 @@ package com.example.navigationbar.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.navigationbar.R;
 
 public class Splash extends AppCompatActivity {
+    Handler handler;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //intro che non ho fatto commint ancora
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity1.class));
+                finish();
+            }
+        },3000);
+
+
+
     }
 }
